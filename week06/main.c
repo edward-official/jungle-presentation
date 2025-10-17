6ba9b0379e38e61bc1bcea82b9db42c7fcedfce8
edwardlee <openmoresome@gmail.com>
2025-10-17 18:30:50 +0900

    2025-10-17

---
+++ week06/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+  pid_t pid;
+  int x = 1;
+  pid = fork();
+  if (pid == 0) {
+    printf("child : x=%d\n", ++x);
+    exit(0);
+  }
+  printf("parent: x=%d\n", --x);
+  exit(0);
+}
