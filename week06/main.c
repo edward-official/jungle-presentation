59bfde68d86a70309958e0cc27057c7d840c56b2
edwardlee <openmoresome@gmail.com>
2025-10-17 19:37:07 +0900

    2025-10-17

--- week06/main.c
+++ week06/main.c
@@ -2,15 +2,25 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#define N 2
+
+
+void unix_error(char *msg) {
+  fprintf(stderr, "%s: %s\n", msg, strerror(errno)); 
+  exit(0); 
+}
+
 
 int main() {
+  int status, i;
   pid_t pid;
-  int x = 1;
-  pid = fork();
-  if (pid == 0) {
-    printf("child : x=%d\n", ++x);
-    exit(0);
+  for (i = 0; i < N; i++) if ((pid = fork()) == 0) exit(100 + i);
+  while ((pid = waitpid(-1, &status, 0)) > 0) {
+    if (WIFEXITED(status)) printf("child %d terminated normally with exit status=%d\n", pid, WEXITSTATUS(status));
+    else printf("child %d terminated abnormally\n", pid);
   }
-  printf("parent: x=%d\n", --x);
+  if (errno != ECHILD) unix_error("waitpid error");
   exit(0);
 }
